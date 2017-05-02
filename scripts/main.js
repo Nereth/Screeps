dbe72d9ecd62d79fb5e129cdb178cb0a979bfe39
jweits <nereth@gmail.com>
2017-05-01 22:38:49 -0500

    Main now checks and adds rooms object to root memory if not already present.

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,11 @@ module.exports.loop = function () {
 		}
 	}
 
+	if (Memory.rooms == null)
+		Memory.rooms = {};
+
+	var monitoredRooms = new Array();
+
 	Object.keys(Game.rooms).forEach(roomName => {
 		new RoomBase(roomName).Update();
 	});
