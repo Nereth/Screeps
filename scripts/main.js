cf6dc437c3d9ab4fd80edfbd1eca909e5012abf3
jweits <nereth@gmail.com>
2017-05-10 00:37:51 -0500

    Added build manager class.

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
 const RoomBase = require('./Room.Base');
 const Factory = require('./Factory');
+const Manager = require('./Manager');
 
 var rooms = null;
 
 module.exports.loop = function () {
-
-
 
 	Object.keys(Game.rooms).forEach(roomName => {
 		new RoomBase(roomName).Update();
 	});
 
 	Factory.Update();
+
+	Manager.Update();
 }
